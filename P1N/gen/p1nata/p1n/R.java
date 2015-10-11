698e10d4d23492a2920f4bfbaba9c5a1335a6c91
Ram Kal <ramk@berkeley.edu>
2015-10-10 17:49:43 -0700

    doors and addnewdoors have features

--- P1N/gen/p1nata/p1n/R.java
+++ P1N/gen/p1nata/p1n/R.java
@@ -8,6 +8,9 @@
 package p1nata.p1n;
 
 public final class R {
+    public static final class array {
+        public static final int dummydatat=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -18,14 +21,14 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
         /** 
          Per the design guidelines, navigation drawers should be between 240dp and 320dp:
          https://developer.android.com/design/patterns/navigation-drawer.html
     
          */
-        public static final int navigation_drawer_width=0x7f040002;
+        public static final int navigation_drawer_width=0x7f050002;
     }
     public static final class drawable {
         public static final int drawer_shadow=0x7f020000;
@@ -33,17 +36,18 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_example=0x7f08000a;
-        public static final int action_settings=0x7f080009;
-        public static final int button1=0x7f080007;
-        public static final int container=0x7f080001;
-        public static final int drawer_layout=0x7f080000;
-        public static final int editText1=0x7f080004;
-        public static final int editText2=0x7f080005;
-        public static final int navigation_drawer=0x7f080002;
-        public static final int section_label=0x7f080008;
-        public static final int textView1=0x7f080003;
-        public static final int textView2=0x7f080006;
+        public static final int action_example=0x7f09000b;
+        public static final int action_settings=0x7f09000a;
+        public static final int button1=0x7f090007;
+        public static final int container=0x7f090001;
+        public static final int drawer_layout=0x7f090000;
+        public static final int editText1=0x7f090004;
+        public static final int editText2=0x7f090005;
+        public static final int navigation_drawer=0x7f090002;
+        public static final int section_label=0x7f090009;
+        public static final int spinner1=0x7f090006;
+        public static final int textView1=0x7f090003;
+        public static final int textView2=0x7f090008;
     }
     public static final class layout {
         public static final int activity_p1_nmain=0x7f030000;
@@ -53,19 +57,20 @@ public final class R {
         public static final int fragment_p1_nmain=0x7f030004;
     }
     public static final class menu {
-        public static final int global=0x7f070000;
-        public static final int p1_nmain=0x7f070001;
+        public static final int global=0x7f080000;
+        public static final int p1_nmain=0x7f080001;
     }
     public static final class string {
-        public static final int action_example=0x7f050005;
-        public static final int action_settings=0x7f050006;
-        public static final int app_name=0x7f050000;
-        public static final int enter_name_of_door=0x7f050008;
-        public static final int enter_new_master_key=0x7f050007;
-        public static final int navigation_drawer_close=0x7f050004;
-        public static final int navigation_drawer_open=0x7f050003;
-        public static final int title_section1=0x7f050001;
-        public static final int title_section2=0x7f050002;
+        public static final int action_example=0x7f060005;
+        public static final int action_settings=0x7f060006;
+        public static final int app_name=0x7f060000;
+        public static final int bluetooth_selecting_hint=0x7f060009;
+        public static final int enter_name_of_door=0x7f060008;
+        public static final int enter_new_master_key=0x7f060007;
+        public static final int navigation_drawer_close=0x7f060004;
+        public static final int navigation_drawer_open=0x7f060003;
+        public static final int title_section1=0x7f060001;
+        public static final int title_section2=0x7f060002;
     }
     public static final class style {
         /** 
@@ -89,10 +94,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }

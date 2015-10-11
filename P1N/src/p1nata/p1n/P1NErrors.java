15c4899c8f1528301484d3bdc5a88bda4c87b64c
Kavi Gupta <kavi@globalmbox.com>
2015-10-10 17:07:01 -0700

    Added a few utility functions for handling bluetooth

---
+++ P1N/src/p1nata/p1n/P1NErrors.java
@@ -0,0 +1,15 @@
+package p1nata.p1n;
+
+public class P1NErrors {
+
+	public static void bluetoothNotFound(P1Nmain main) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void bluetoothNotEnabled(P1Nmain p1Nmain) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
